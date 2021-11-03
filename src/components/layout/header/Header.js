bbf27d6f30b58a1c3f93c83fbbbc3d5eec19a9ca
Isela <isela.v.alarcon@gmail.com>
2021-11-03 15:26:39 +0100

    starting the fourth react project

---
+++ src/components/layout/header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//IMPORT STYLES
+import { Hello } from "./Header.styles";
+
+const HelloWorld = () => {
+  return <Hello>Hello</Hello>;
+};
+
+export default HelloWorld;
---
+++ src/components/layout/header/Header.styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Hello = styled.div`
+  font-size: 14;
+  color: black;
+`;
