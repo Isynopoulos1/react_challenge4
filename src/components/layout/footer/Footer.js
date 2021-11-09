f4570e88d6e936ed0bba5b234c2aa310a41f7737
Isela <isela.v.alarcon@gmail.com>
2021-11-09 21:48:37 +0100

    styling the list

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,6 +1,13 @@
 import React from "react";
 
-import { FooterStyle, FooterBar } from "./Footer.styles";
+import {
+  FooterStyle,
+  FooterBar,
+  ServicesList,
+  List1,
+  List2,
+  List3
+} from "./Footer.styles";
 
 //IMPORT COMPONENTS
 import Logo from "../../elements/Logo/Logo";
@@ -10,6 +17,30 @@ const Footer = () => {
     <FooterStyle>
       <Logo img="https://ik.imagekit.io/ppayaz/ppayaz-website/Captura_de_Pantalla_2021-11-09_a_la_s__15.29.02_OJrQih9Bau.png?updatedAt=1636468167564" />
       <FooterBar />
+      <ServicesList>
+        <List1>
+          <ul>Encontre imóveis</ul>
+          <ul>Venda</ul>
+          <ul>Aluguel</ul>
+          <ul>Lançamentos</ul>
+          <ul>Alugar imóvel barato</ul>
+          <ul>Comprar imóvel barato</ul>
+          <ul>Financiamento Imobiliário</ul>
+        </List1>
+        <List2>
+          <ul>Anuncie imóveis</ul>
+          <ul>ZAP Pro</ul>
+          <ul>ZAP Estima</ul>
+          <ul>Planos de Anúncio</ul>
+          <ul>Política de Qualidade</ul>
+          <ul>Venda seu Imóvel</ul>
+        </List2>
+        <List3>
+          <ul>Institucional</ul>
+          <ul>Quem somos</ul>
+          <ul>Trabalhe conosco</ul>
+        </List3>
+      </ServicesList>
     </FooterStyle>
   );
 };
--- src/components/layout/footer/Footer.styles.js
+++ src/components/layout/footer/Footer.styles.js
@@ -20,3 +20,34 @@ export const FooterBar = styled.div`
   border-bottom: 1px solid white;
   margin-bottom: 30px;
 `;
+
+export const ServicesList = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  grid-gap: 100px;
+  flex-direction: flex;
+  padding-left: 24px;
+`;
+
+export const List1 = styled.ul`
+  font-size: 15px;
+  font-family: helvetica;
+  color: white;
+  font-weight: lighter;
+  margin-right: 150px;
+`;
+export const List2 = styled.ul`
+  font-size: 15px;
+  font-family: helvetica;
+  color: white;
+  font-weight: lighter;
+  margin-right: 150px;
+`;
+export const List3 = styled.ul`
+  font-size: 15px;
+  font-family: helvetica;
+  color: white;
+  font-weight: lighter;
+  margin-right: 150px;
+`;
