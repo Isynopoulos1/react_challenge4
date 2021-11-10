2a36ad875320d5320b5ebfa8195bfdf645b5314d
Isela <isela.v.alarcon@gmail.com>
2021-11-10 18:33:18 +0100

    setting the cards

--- src/components/layout/main/Main.js
+++ src/components/layout/main/Main.js
@@ -7,65 +7,65 @@ import { MainContainer, Tittle, MainText, Subtext } from "./Main.styles";
 import Items from "../../elements/Items/Items";
 
 const Main = () => {
-  const appartments1 = [
+  const list1 = [
     {
       image:
         "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
-      title: "sfkhjaf",
-      price: "$12340",
-      detail: "asfasf"
+      title: "Aldus PageMaker",
+      price: 312340,
+      detail: "30m²"
     },
     {
       image:
-        "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
-      title: "sfkhjaf",
-      price: 12123,
-      detail: "asfasf"
+        "https://ik.imagekit.io/ppayaz/react_challenge3/kam-idris-_HqHX3LBN18-unsplash_maHESzct_.jpeg?updatedAt=1636407336870",
+      title: "Richard McClintock",
+      price: 93889,
+      detail: "37m²"
     },
     {
       image:
-        "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
-      title: "sfkhjaf",
-      price: 12123,
-      detail: "asfasf"
+        "https://ik.imagekit.io/ppayaz/react_challenge3/sven-brandsma-GmRiN7tVW1w-unsplash_NwgTrwvGXA.jpeg?updatedAt=1636407346133",
+      title: "Malorum",
+      price: 34546,
+      detail: "80m²"
     },
     {
       image:
-        "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
-      title: "sfkhjaf",
-      price: 12123,
-      detail: "asfasf"
+        "https://ik.imagekit.io/ppayaz/react_challenge3/andy-art-zwZpdhoTbU0-unsplash_bQYE-PLO7.jpeg?updatedAt=1636407272596",
+      title: "Cicero",
+      price: 204973,
+      detail: "34m²"
     }
   ];
 
-  const appartments2 = [
+  const list2 = [
     {
       image:
-        "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
-      title: "sfkhjaf",
+        "https://ik.imagekit.io/ppayaz/react_challenge3/amira-aboalnaga-f9O-1eKGlQM-unsplash_ScFr8tYxj.jpeg?updatedAt=1636128128331",
+      title: "Finibus Bonorum ",
       price: 12123,
-      detail: "asfasf"
+      detail: "40m²"
     },
     {
       image:
-        "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
-      title: "sfkhjaf",
+        "https://ik.imagekit.io/ppayaz/react_challenge3/naomi-hebert-2dcYhvbHV-M-unsplash_2w9hxAmOkj.jpeg?updatedAt=1636128134208",
+      title: "H. Rackham",
       price: 12123,
-      detail: "asfasf"
+      detail: "30m²"
     },
     {
       image:
-        "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
-      title: "sfkhjaf",
+        "https://ik.imagekit.io/ppayaz/react_challenge3/francesca-tosolini-w1RE0lBbREo-unsplash_P0aVWLYt9.jpeg?updatedAt=1636407263250",
+      title: "Renaissance",
       price: 12123,
-      detail: "asfasf"
+      detail: "20m²"
     },
     {
       image:
-        "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
-      title: "sfkhjaf",
+        "https://ik.imagekit.io/ppayaz/react_challenge3/kara-eads-L7EwHkq1B2s-unsplash_tvjB5Fvc6.jpeg?updatedAt=1636128030456",
+      title: "Aldus",
       price: 12123,
-      detail: "asfasf"
+      detail: "35m²"
     }
   ];
 
@@ -84,8 +84,8 @@ const Main = () => {
           </Subtext>
         </Tittle>
       </MainContainer>
-      <Items list={appartments1} />
-      <Items list={appartments2} />
+      <Items list={list1} />
+      <Items list={list2} />
     </main>
   );
 };
